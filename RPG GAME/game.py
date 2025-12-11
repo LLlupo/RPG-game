09c295565830d80ac760415b102a6a588973e255
LLlupo <127114577+LLlupo@users.noreply.github.com>
2025-12-11 16:31:54 +1000

    Update game.py

--- RPG GAME/game.py
+++ RPG GAME/game.py
@@ -1,5 +1,6 @@
 from random import *
-
+import sys
+assert sys.version_info
 Сила = 'strength'
 # wiki = {
 #     'armor': {
@@ -15,40 +16,44 @@ from random import *
 #     'poison': ["Розовая слизь, Голубая слизь, Красная слизь, Кровь лесной дриады, Ягодный сок"],
 # }
 wiki = {
-    'Плащ шамана-гоблина': ['ПлАщ, пРоПИТанНЫй ИСКусСТвоМ чуДОдейСТвА ГоБЛиНОв! ЕСЛи оНО У нИХ еСТь? ХыХЫ',
+    'Плащ шамана-гоблина': ['- ПлАщ, пРоПИТанНЫй ИСКусСТвоМ чуДОдейСТвА ГоБЛиНОв! ЕСЛи оНО У нИХ еСТь? ХыХЫ',
                             [["Урон", 4], ['Защита', 3]],
                             'Броня'],
-    'Диадема лесной дриады': ["Цветочная корона жестокой хранительницы леса.",
+    'Диадема лесной дриады': ["- Цветочная корона жестокой хранительницы леса.",
                               [['Урон', 6], ['Ловкость', 5]],
                               'Броня'],
-    'Маска стеснительного орка': ["Чёрная маска предводителя орков.",
+    'Маска стеснительного орка': ["- Чёрная маска предводителя орков.",
                                   [['Защита', 7], ['Очки здоровья', 4]],
                                   'Броня'],
-    'Накидка из шкуры оленя-перевёртыша': ["ОТ еЁ вида МНе нЕМНогО жУТкО...",
+    'Накидка из шкуры оленя-перевёртыша': ["- ОТ еЁ вида МНе нЕМНогО жУТкО...",
                                            [['Урон', 5], ['Очки здоровья', 4], ['Защита', 5]],
                                            'Броня'],
-    'Пояс разбойника Первого': ["ИлИ ВТОРогО? ТЫ ПоМНиШЬ?",
-                                [['Урон', 6], ['Очки здоровья', 2], ['Защита', 6]],
+    'Пояс разбойника Первого': ["- ИлИ ВТОРогО? ТЫ ПоМНиШЬ?",
+                                [['Урон', 8], ['Очки здоровья', 2], ['Защита', 6]],
                                 'Броня'],
     'Перчатки разбойника Второго': ["Ну тОЧНо ВтОрОГО.",
-                                    [['Урон', 5], ['Ловкость', 3], ['Защита', 6]],
+                                    [['Урон', 10], ['Ловкость', 3], ['Защита', 6]],
                                     'Броня'],
+    'Книга заклинаний': ['КАкаЯ кНижкАа :З', ['Урон', 6], 'Орудие']
 }
 wiki_monsters = {
-    "Гоблин":[(3,6), (3,6), (2,5), (2,5), (2,4), 15],
-    "Гоблин-шаман":[(3,6), (4,7), (2,4), (2,5), (3,5), 20],
-    "Орк":[(5,9), (6,8), (2,4), (4,6), (13,16), 25],
-    "Стеснительный орк":[(10,16), (7,10), (2,4), (7,12), (22,30), 34],
-    "Голубая слизь":[(5,8), (3,5), (13,18), (2,5), (8,15), 34],
-    "Розовая слизь":[(13,16), (3,5), (2,5), (2,5), (8,15), 30],
-    "Красная слизь":[(3,6), (8,10), (2,5), (2,5), (8,15), 25],
-    "Олень-перевёртыш":[(15,20), (8,14), (7,10), (3,7), (13,20), 35],
+    "Гоблин": [(3,6), (3,6), (2,5), (2,5), (2,4), 15],
+    "Гоблин-шаман": [(3,6), (4,7), (2,4), (2,5), (3,5), 20],
+    "Орк": [(5,9), (6,8), (2,4), (4,6), (13,16), 25],
+    "Стеснительный орк": [(10,16), (7,10), (2,4), (7,12), (22,30), 34],
+    "Голубая слизь": [(5,8), (3,5), (13,18), (2,5), (8,15), 34],
+    "Розовая слизь": [(13,16), (3,5), (2,5), (2,5), (8,15), 30],
+    "Красная слизь": [(3,6), (8,10), (2,5), (2,5), (8,15), 25],
+    "Олень-перевёртыш": [(15,20), (8,14), (7,10), (3,7), (13,20), 35],
     "Разбойник":[],
     "Разбойник Первый":[],
     "Разбойник Второй":[],
     "Дриада":[],
     "Шипастая Дриада":[],
     "???":[],
+}
+wiki_consume = {
+    "Ягодный сок": [['Очки здоровья', 4], ['Ловкость', 1]]
 }
 class Player:
     def __init__(self):
@@ -58,7 +63,7 @@ class Player:
         self.equipment = {
             'Броня':'Диадема лесной дриады',
             'Аксессуар': '-',
-            'Оружие': '-',
+            'Орудие': '-',
             'poison': '',
         }
         self.inventory = ['Ягодный сок', 'Книга заклинаний', 'Плащ шамана-гоблина']
@@ -85,12 +90,13 @@ class Player:
     def get_level(self):
         self.level += 1
         self.free_points += self.level * 5
-        match self.level:
-            case 0: self.exp_next = 10
-            case 1: self.exp_next = 50
-            case 2: self.exp_next = 100
-            case 3: self.exp_next = 500
-            case 4: self.exp_next = 1000
+        # match self.level:
+        #     case 0: self.exp_next = 10
+        #     case 1: self.exp_next = 50
+        #     case 2: self.exp_next = 100
+        #     case 3: self.exp_next = 500
+        #     case 4: self.exp_next = 1000
+        self.exp_next = 10
         self.exp_bar = (self.experience // (self.exp_next//10))*'▓' + (10-len((self.experience // (self.exp_next//10))*'▓'))*'░'
     def show_all(self):
         print('\n'
@@ -105,7 +111,7 @@ class Player:
             f"⠀⠀⠀⠘⠶⠟⠀⠻⠶⠃⠀⠀⠀⠀    ・Защита: {self.stats['Защита']}\n"
         )
         print(f'▐ Броня: {self.equipment["Броня"]}  '
-              f'▐ Оружие: {self.equipment["Оружие"]}  '
+              f'▐ Орудие: {self.equipment["Орудие"]}  '
               f'▐ Аксессуар: {self.equipment["Аксессуар"]}\n')
         print('     ✦ Инвентарь ✦')
         for i, item in enumerate(self.inventory):
@@ -115,11 +121,11 @@ class Player:
         print(
             f"⠀⠀⠀⠀⠀⠀⠙⣷⠀⠀⠀⠀⠀⠀  \n"
             f"⠀⠀⠀⣤⠿⠿⠿⠿⠿⣤⠀⠀⠀⠀ \n"
-            f"⠀⢰⡟⠀⠀⠀⠀⠀⠀⠀⢻⣆⠀⠀    ・Очки здоровья: {self.stats["Очки здоровья"]}\n"
-            f"⠸⣤⠀⠀⣿⠀⠀⠀⣿⠀⠀⣤⠇⠀    ・Сила: {self.stats["Сила"]}\n"
-            f"⠀ ⠈⠷⢦⠀⠀⠀⡶⠾⠁⠀⠀     ・Магия: {self.stats["Магия"]}\n"
-            f"⠀⠀⠀⣰⠛⠋⠉⠙⠛⣆⠀⠀⠀⠀    ・Ловкость: {self.stats["Ловкость"]}\n"
-            f"⠀⢠⡟⢰ ⠀⠀⠀ ⡆⢻⡄⠀⠀    ・Защита: {self.stats["Защита"]}\n"
+            f"⠀⢰⡟⠀⠀⠀⠀⠀⠀⠀⢻⣆⠀⠀    ・Очки здоровья: {self.stats['Очки здоровья']}\n"
+            f"⠸⣤⠀⠀⣿⠀⠀⠀⣿⠀⠀⣤⠇⠀    ・Сила: {self.stats['Сила']}\n"
+            f"⠀ ⠈⠷⢦⠀⠀⠀⡶⠾⠁⠀⠀     ・Магия: {self.stats['Магия']}\n"
+            f"⠀⠀⠀⣰⠛⠋⠉⠙⠛⣆⠀⠀⠀⠀    ・Ловкость: {self.stats['Ловкость']}\n"
+            f"⠀⢠⡟⢰ ⠀⠀⠀ ⡆⢻⡄⠀⠀    ・Защита: {self.stats['Защита']}\n"
             f"⠀⠀⠀⢸⠀⢰⠉⡆⠀⡇⠀⠀⠀⠀ \n"
             f"⠀⠀⠀⠘⠶⠟⠀⠻⠶⠃⠀⠀⠀⠀ \n"
         )
@@ -160,9 +166,9 @@ class Player:
         self.stats['health'] = max(self.stats['health'] - damage, 0)
         print(f'Существо {enemy.kind} атакует!')
         print(f"Вам нанесли {damage} урона. У вас осталось {self.stats['health']} ОЗ.")
-    def show_inventory(self):
+    def show_inventory(self, inventory):
         print('     ✦ Инвентарь ✦')
-        for i, item in enumerate(self.inventory):
+        for i, item in enumerate(inventory):
             print(i+1,'-', item)
     def unequip(self, item):
         print(f"\n- Мы СнЯЛиИ: {item}")
@@ -177,7 +183,7 @@ class Player:
             self.inventory.remove(item)
             print(f"\n- ОБнОВка!! {item}")
         elif self.equipment[wiki[item][-1]] != '-':
-            yesno = 0
+            yesno = ''
             while yesno not in ['1','2']:
                 print(f"- ЭтА каТЕгОРИя уЖЕ зАНЯтА: {self.equipment[wiki[item][-1]]}. ПОмЕнЯеМ? (1 - Да, 2 - Нет)")
                 yesno = input()
@@ -189,6 +195,15 @@ class Player:
         for i in wiki[item][1]:
             print(f"- ХарАКтеРИстИКА {i[0]} ВоЗРОсЛа нА {i[1]}! :D")
             self.stats[i[0]] += i[1]
+    def consume(self, item):
+        yesno = ''
+        while yesno not in ['1', '2']:
+            yesno = input(f'- ТоЧнО МОжНо {item}? (1 - Да, 2 - Нет)\n')
+        if yesno == "1":
+            for i in wiki_consume[item]:
+                print(f"- ХарАКтеРИстИКА {i[0]} ВоЗРОсЛа нА {i[1]}! :D")
+                self.stats[i[0]] += i[1]
+            self.inventory.remove(item)
 player = Player()
 print("- ПРиВЕи! ДоБРо ПОжаЛоВать в МИр! КтО я? НУ... КтО-тО? БЕз ПОНЯтиЯ, КтО Я. ЗаТО ЗнАЮ, ЧТО ты - ЧеЛОвЕкоПоДОбнАя сЛИзЬ!\n")
 player.get_stats()
@@ -228,17 +243,43 @@ def menu(player):
         cur_mon = [Creature(i, *wiki_monsters[i]) for i in cur_mon]
         for i in cur_mon:
             i.show_stats()
-    mon_meet()
-menu(player)
+    def choice_menu():
+        player_choice = '1'
+        while player_choice not in ['1','2','3']:
+            player_choice = input("- ЧтО дЕлАТь БуДЕм? (1 - Инвентарь, 2 - Атака, 3 - Уклонение)\n")
+        if player_choice == '1':
+            inventory()
+    def inventory():
+        talk = "- ВОтаКвОТ" if randint(0,1)==0 else "- НУ ДавАЙ поСМОтРиМ"
+        print(talk)
+        player.show_all()
+        player_inventory_choice = '2'
+        while player_inventory_choice not in ['1', '2', '3']:
+            player_inventory_choice = input("- ЧтО ТеПЕрЬ? (1 - Поглотить, 2 - Надеть, 3 - Ничего)\n")
+        if player_inventory_choice == '1':
+            print("- ЧТо КуШАеМ? :P\n")
+            player.show_inventory(player.inventory)
+            eat_item = '1'
+            # eat_item = input()
+            player.consume(player.inventory[int(eat_item)-1])
+            return inventory()
+        elif player_inventory_choice == "2":
+            print("- ВОт наШ ГАрдЕроБ!! ЧТо НаДЕнЕм? :^\n")
+            usable_inventory = [i for i in player.inventory if i in wiki.keys()]
+            player.show_inventory(usable_inventory)
+            puton_item = '1'
+            # puton_item = input()
+            player.equip(usable_inventory[int(puton_item)-1])
+            # return inventory()
+        else:
+            return choice_menu()
 
-# common_goblin = Creature('Гоблин',(3,6),(3,6),(2,5),(2,5),(3,5),15)
-# mage_goblin = Creature("Гоблин-шаман",(3,6),(4,7),(2,4),(2,5), (4,6),20)
-# magge = Creature("Стеснительный орк", *wiki_monsters['Стеснительный орк'])
-# magge.show_stats()
-# deer_skinwalker = Creature("Олень-перевёртыш", (15,20), (8,14), (7,10), (3,7), 30)
-# mage_goblin.show_stats()
-# player.examine(common_goblin)
+
+    mon_meet()
+    choice_menu()
+
 player.show_all()
+menu(player)
 # for i in wiki_monsters.keys():
 #     cr = Creature(i, *wiki_monsters[i])
 #     cr.show_stats()
